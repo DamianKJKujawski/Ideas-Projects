5cf3e0ec6f06cb6435aa7d6216053ca0a87a6e57
Damian <damian.kujawski@proton.me>
2024-04-25 12:11:50 +0200

    Added new project
    Project: MVVM implementation

--- BTree_Database/UnitTest/obj/Debug/net6.0/UnitTest.AssemblyInfo.cs
+++ BTree_Database/UnitTest/obj/Debug/net6.0/UnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+140f12b2470a28030580d8deac392be34d0cfc7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
